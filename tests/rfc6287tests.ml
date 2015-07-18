9b7869c4529c312b12c3055c5181e0c12807f52c
Stefan Grundmann <sg2342@googlemail.com>
2015-07-18 12:42:38 +0000

    style

--- tests/rfc6287tests.ml
+++ tests/rfc6287tests.ml
@@ -32,164 +32,127 @@ let istr l i =
 let assert_cs_eq_s s cs =
   assert_equal s (Cstruct.to_string cs)
 
-let o1 ctx =
-  let suite = suite ctx in
-  let key = key `K20 in
-  let l = ["237653"; "243178"; "653583"; "740991"; "608993"; "388898";
-           "816933"; "224598"; "750600"; "294470"] in
-  List.iteri (fun i r ->
-      let q = istr 8 i in
-      let o = Rfc6287.gen ~suite ~key q in
-      assert_cs_eq_s r o) l
-
-let o2 ctx =
-  let suite = suite ctx in
-  let key = key `K32 in
-  let p = pinhash in
-  let q = "12345678" in
-  let l = ["65347737"; "86775851"; "78192410"; "71565254"; "10104329";
-           "65983500"; "70069104"; "91771096"; "75011558"; "08522129"] in
-  List.iteri (fun i r ->
-      let c = Int64.of_int i in
-      let o = Rfc6287.gen ~suite ~key ~c ~p q in
-      assert_cs_eq_s r o) l
-
-let o3 ctx =
-  let suite = suite ctx in
-  let key = key `K32 in
-  let p = pinhash in
-  let l = ["83238735"; "01501458"; "17957585"; "86776967"; "86807031"] in
-  List.iteri (fun i r ->
-      let q = istr 8 i in
-      let o = Rfc6287.gen ~suite ~key ~p q in
-      assert_cs_eq_s r o) l
-
-let o4 ctx =
-  let suite = suite ctx in
-  let key = key `K64 in
-  let l = ["07016083"; "63947962"; "70123924"; "25341727"; "33203315";
-           "34205738"; "44343969"; "51946085"; "20403879"; "31409299"] in
-  List.iteri (fun i r ->
-      let c = Int64.of_int i in
-      let q = istr 8 i in
-      let o = Rfc6287.gen ~suite ~key ~c q in
-      assert_cs_eq_s r o) l
-
-let o5 ctx =
-  let suite = suite ctx in
-  let key = key `K64 in
-  let t = timestamp in
-  let l =["95209754"; "55907591"; "22048402"; "24218844"; "36209546"] in
-  List.iteri (fun i r ->
-      let q = istr 8 i in
-      let o = Rfc6287.gen ~suite ~key ~t q in
-      assert_cs_eq_s r o) l
-
-let s1 ctx =
-  let suite = suite ctx in
-  let key = key `K32 in
-  let l = [("SIG10000","53095496");
-           ("SIG11000","04110475");
-           ("SIG12000","31331128");
-           ("SIG13000","76028668");
-	   ("SIG14000","46554205")] in
-  List.iter (fun (q, r) ->
-      let o = Rfc6287.gen ~suite ~key q in
-      assert_cs_eq_s r o) l
-
-let s2 ctx =
-  let suite = suite ctx in
-  let key = key `K64 in
-  let t = timestamp in
-  let l = [("SIG1000000","77537423");
-           ("SIG1100000","31970405");
-           ("SIG1200000","10235557");
-	   ("SIG1300000","95213541");
-           ("SIG1400000","65360607")] in
-  List.iter (fun (q, r) ->
-      let o = Rfc6287.gen ~suite ~key ~t q in
-      assert_cs_eq_s r o) l
-
-let m1 ctx =
-  let suite = suite ctx in
-  let key = key `K32 in
-  let l = [("CLI22220SRV11110","28247970");
-           ("CLI22221SRV11111","01984843");
-	   ("CLI22222SRV11112","65387857");
-           ("CLI22223SRV11113","03351211");
-	   ("CLI22224SRV11114","83412541");
-           ("SRV11110CLI22220","15510767");
-           ("SRV11111CLI22221","90175646");
-           ("SRV11112CLI22222","33777207");
-           ("SRV11113CLI22223","95285278");
-           ("SRV11114CLI22224","28934924")] in
-  List.iter (fun (q, r) ->
-      let o = Rfc6287.gen ~suite ~key q in
-      assert_cs_eq_s r o) l
-
-let m2 ctx =
-  let suite = suite ctx in
-  let key = key `K64 in
-  let l = [("CLI22220SRV11110","79496648");
-           ("CLI22221SRV11111","76831980");
-           ("CLI22222SRV11112","12250499");
-           ("CLI22223SRV11113","90856481");
-           ("CLI22224SRV11114","12761449")] in
-  List.iter (fun (q, r) ->
-      let o = Rfc6287.gen ~suite ~key q in
-      assert_cs_eq_s r o) l
-
-let m3 ctx =
-  let suite = suite ctx in
-  let key = key `K64 in
-  let p = pinhash in
-  let l = [("SRV11110CLI22220","18806276");
-	   ("SRV11111CLI22221","70020315");
-	   ("SRV11112CLI22222","01600026");
-	   ("SRV11113CLI22223","18951020");
-	   ("SRV11114CLI22224","32528969")] in
-  List.iter (fun (q, r) ->
-      let o = Rfc6287.gen ~suite ~key ~p q in
-      assert_cs_eq_s r o) l
-
-let invalid_suite ctx =
-  match Rfc6287.t_of_string (suitestring ctx) with
-  | None -> ()
-  | _ -> assert_failure "parsed invalid suite!"
-
-let string_of_t ctx =
-  let ss = suitestring ctx in
-  let s = suite ctx in
-  assert_equal ss (Rfc6287.string_of_t s)
-
-let challenge ctx =
-  let s = suite ctx in
-  let _ = Rfc6287.challenge s in
-  ()
-
-let c_gen_di_missmatch ctx =
-  let suite = suite ctx in
-  let key = key `K32 in
-  let q = Rfc6287.challenge suite in
-  try let _ = Rfc6287.gen ~suite ~key q in () with
-  | _ -> ()
-
-let c_gen_session_data ctx =
-  let suite = suite ctx in
-  let key = key `K20 in
-  let q = Rfc6287.challenge suite in
-  let s = Cstruct.create 235 in
-  try let _ = Rfc6287.gen ~suite ~key ~s q in () with
-  | _ -> ()
-
-let c_gen_cf_fill ctx =
-  let suite = suite ctx in
-  let key = key `K20 in
-  let q = "00000000" in
-  try let _ = Rfc6287.gen ~suite ~key q in () with
-  | _ -> ()
 
 let known_answer =
+  let o1 ctx =
+    let suite = suite ctx in
+    let key = key `K20 in
+    let l = ["237653"; "243178"; "653583"; "740991"; "608993"; "388898";
+             "816933"; "224598"; "750600"; "294470"] in
+    List.iteri (fun i r ->
+        let q = istr 8 i in
+        let o = Rfc6287.gen ~suite ~key q in
+        assert_cs_eq_s r o) l in
+
+  let o2 ctx =
+    let suite = suite ctx in
+    let key = key `K32 in
+    let p = pinhash in
+    let q = "12345678" in
+    let l = ["65347737"; "86775851"; "78192410"; "71565254"; "10104329";
+             "65983500"; "70069104"; "91771096"; "75011558"; "08522129"] in
+    List.iteri (fun i r ->
+        let c = Int64.of_int i in
+        let o = Rfc6287.gen ~suite ~key ~c ~p q in
+        assert_cs_eq_s r o) l in
+
+  let o3 ctx =
+    let suite = suite ctx in
+    let key = key `K32 in
+    let p = pinhash in
+    let l = ["83238735"; "01501458"; "17957585"; "86776967"; "86807031"] in
+    List.iteri (fun i r ->
+        let q = istr 8 i in
+        let o = Rfc6287.gen ~suite ~key ~p q in
+        assert_cs_eq_s r o) l in
+
+  let o4 ctx =
+    let suite = suite ctx in
+    let key = key `K64 in
+    let l = ["07016083"; "63947962"; "70123924"; "25341727"; "33203315";
+             "34205738"; "44343969"; "51946085"; "20403879"; "31409299"] in
+    List.iteri (fun i r ->
+        let c = Int64.of_int i in
+        let q = istr 8 i in
+        let o = Rfc6287.gen ~suite ~key ~c q in
+        assert_cs_eq_s r o) l in
+
+  let o5 ctx =
+    let suite = suite ctx in
+    let key = key `K64 in
+    let t = timestamp in
+    let l =["95209754"; "55907591"; "22048402"; "24218844"; "36209546"] in
+    List.iteri (fun i r ->
+        let q = istr 8 i in
+        let o = Rfc6287.gen ~suite ~key ~t q in
+        assert_cs_eq_s r o) l in
+
+  let s1 ctx =
+    let suite = suite ctx in
+    let key = key `K32 in
+    let l = [("SIG10000","53095496");
+             ("SIG11000","04110475");
+             ("SIG12000","31331128");
+             ("SIG13000","76028668");
+	     ("SIG14000","46554205")] in
+    List.iter (fun (q, r) ->
+        let o = Rfc6287.gen ~suite ~key q in
+        assert_cs_eq_s r o) l in
+
+  let s2 ctx =
+    let suite = suite ctx in
+    let key = key `K64 in
+    let t = timestamp in
+    let l = [("SIG1000000","77537423");
+             ("SIG1100000","31970405");
+             ("SIG1200000","10235557");
+	     ("SIG1300000","95213541");
+             ("SIG1400000","65360607")] in
+    List.iter (fun (q, r) ->
+        let o = Rfc6287.gen ~suite ~key ~t q in
+        assert_cs_eq_s r o) l in
+
+  let m1 ctx =
+    let suite = suite ctx in
+    let key = key `K32 in
+    let l = [("CLI22220SRV11110","28247970");
+             ("CLI22221SRV11111","01984843");
+	     ("CLI22222SRV11112","65387857");
+             ("CLI22223SRV11113","03351211");
+	     ("CLI22224SRV11114","83412541");
+             ("SRV11110CLI22220","15510767");
+             ("SRV11111CLI22221","90175646");
+             ("SRV11112CLI22222","33777207");
+             ("SRV11113CLI22223","95285278");
+             ("SRV11114CLI22224","28934924")] in
+    List.iter (fun (q, r) ->
+        let o = Rfc6287.gen ~suite ~key q in
+        assert_cs_eq_s r o) l in
+
+  let m2 ctx =
+    let suite = suite ctx in
+    let key = key `K64 in
+    let l = [("CLI22220SRV11110","79496648");
+             ("CLI22221SRV11111","76831980");
+             ("CLI22222SRV11112","12250499");
+             ("CLI22223SRV11113","90856481");
+             ("CLI22224SRV11114","12761449")] in
+    List.iter (fun (q, r) ->
+        let o = Rfc6287.gen ~suite ~key q in
+        assert_cs_eq_s r o) l in
+
+  let m3 ctx =
+    let suite = suite ctx in
+    let key = key `K64 in
+    let p = pinhash in
+    let l = [("SRV11110CLI22220","18806276");
+	     ("SRV11111CLI22221","70020315");
+	     ("SRV11112CLI22222","01600026");
+	     ("SRV11113CLI22223","18951020");
+	     ("SRV11114CLI22224","32528969")] in
+    List.iter (fun (q, r) ->
+        let o = Rfc6287.gen ~suite ~key ~p q in
+        assert_cs_eq_s r o) l in
   ["one_way" >::: ["OCRA-1:HOTP-SHA1-6:QN08" >::o1;
                    "OCRA-1:HOTP-SHA256-8:C-QN08-PSHA1" >:: o2;
                    "OCRA-1:HOTP-SHA256-8:QN08-PSHA1" >:: o3;
@@ -200,34 +163,73 @@ let known_answer =
    "mutual" >::: ["OCRA-1:HOTP-SHA256-8:QA08" >:: m1;
                   "OCRA-1:HOTP-SHA512-8:QA08" >:: m2;
                   "OCRA-1:HOTP-SHA512-8:QA08-PSHA1" >:: m3]]
+
+
 let coverage =
-  [ "t_of_string" >::: ["this_is_not_a_suite_string" >:: invalid_suite;
-                        "OCRA-1::QA08" >:: invalid_suite;
-                        "OCRA-1:::QA08" >:: invalid_suite;
-                        "OCRA-1:HOTP-SHA1-0:QN08-T01X" >:: invalid_suite;
-                        "OCRA-1:HOTP-SHA1-0:QN08-T91S" >:: invalid_suite;
-                        "OCRA-1:HOTP-SHA1-0:QN08-T91H" >:: invalid_suite;
-                        "OCRA-1:HOTP-SHA1-0:QN08-T" >:: invalid_suite;
-                        "OCRA-1:HOTP-SHA1-0:QH08-S028---" >:: invalid_suite;
-                        "OCRA-1:HOTP-SHA1-0:QX08" >:: invalid_suite;
-                        "OCRA-1:HOTP-SHA1-0:C-" >:: invalid_suite;
-                        "OCRA-1:HOTP-SHA1-0:C" >:: invalid_suite;
-                        "OCRA-1:HOTP-SHA1-0:" >:: invalid_suite;
-                        "OCRA-1:HOTP-SHA1-1:QA08" >:: invalid_suite;
-                        "OCRA-1:HOTP-SHA-0:QN08" >:: invalid_suite;
-                        "OCRA-1:HOTP-SHA1-a0:QN08" >:: invalid_suite;
-                        "OCRA-1:HTOP-SHA1-0:QA-08" >:: invalid_suite];
-    "string_of_t" >::: ["OCRA-1:HOTP-SHA512-0:C-QH16-T14H" >:: string_of_t];
-    "challenge" >::: ["OCRA-1:HOTP-SHA256-0:QA10" >:: challenge;
-                      "OCRA-1:HOTP-SHA256-0:QN10" >:: challenge;
-                      "OCRA-1:HOTP-SHA256-0:QH10" >:: challenge];
-    "gen" >::: ["OCRA-1:HOTP-SHA256-0:C-QH10" >:: c_gen_di_missmatch;
-                "OCRA-1:HOTP-SHA256-0:QH10-PSHA1" >:: c_gen_di_missmatch;
-                "OCRA-1:HOTP-SHA256-0:QH10-T1H" >:: c_gen_di_missmatch;
-                "OCRA-1:HOTP-SHA256-0:QH10-S123" >:: c_gen_di_missmatch;
-                "OCRA-1:HOTP-SHA1-0:QH10-S235" >:: c_gen_session_data;
-                "OCRA-1:HOTP-SHA1-10:QH10-S235" >:: c_gen_session_data;
-                "OCRA-1:HOTP-SHA1-10:QN08" >:: c_gen_cf_fill]]
+  let invalid_suite ctx =
+    match Rfc6287.t_of_string (suitestring ctx) with
+    | None -> ()
+    | _ -> assert_failure "parsed invalid suite!" in
+
+  let string_of_t ctx =
+    let ss = suitestring ctx in
+    let s = suite ctx in
+    assert_equal ss (Rfc6287.string_of_t s) in
+
+  let challenge ctx =
+    let s = suite ctx in
+    let _ = Rfc6287.challenge s in
+    () in
+
+  let gen_di_missmatch ctx =
+    let suite = suite ctx in
+    let key = key `K32 in
+    let q = Rfc6287.challenge suite in
+    try let _ = Rfc6287.gen ~suite ~key q in () with
+    | _ -> () in
+
+  let gen_session_data ctx =
+    let suite = suite ctx in
+    let key = key `K20 in
+    let q = Rfc6287.challenge suite in
+    let s = Cstruct.create 235 in
+    try let _ = Rfc6287.gen ~suite ~key ~s q in () with
+    | _ -> () in
+
+  let gen_cf_fill ctx =
+    let suite = suite ctx in
+    let key = key `K20 in
+    let q = "00000000" in
+    try let _ = Rfc6287.gen ~suite ~key q in () with
+    | _ -> () in
+
+  ["t_of_string" >::: ["this_is_not_a_suite_string" >:: invalid_suite;
+                       "OCRA-1::QA08" >:: invalid_suite;
+                       "OCRA-1:::QA08" >:: invalid_suite;
+                       "OCRA-1:HOTP-SHA1-0:QN08-T01X" >:: invalid_suite;
+                       "OCRA-1:HOTP-SHA1-0:QN08-T91S" >:: invalid_suite;
+                       "OCRA-1:HOTP-SHA1-0:QN08-T91H" >:: invalid_suite;
+                       "OCRA-1:HOTP-SHA1-0:QN08-T" >:: invalid_suite;
+                       "OCRA-1:HOTP-SHA1-0:QH08-S028---" >:: invalid_suite;
+                       "OCRA-1:HOTP-SHA1-0:QX08" >:: invalid_suite;
+                       "OCRA-1:HOTP-SHA1-0:C-" >:: invalid_suite;
+                       "OCRA-1:HOTP-SHA1-0:C" >:: invalid_suite;
+                       "OCRA-1:HOTP-SHA1-0:" >:: invalid_suite;
+                       "OCRA-1:HOTP-SHA1-1:QA08" >:: invalid_suite;
+                       "OCRA-1:HOTP-SHA-0:QN08" >:: invalid_suite;
+                       "OCRA-1:HOTP-SHA1-a0:QN08" >:: invalid_suite;
+                       "OCRA-1:HTOP-SHA1-0:QA-08" >:: invalid_suite];
+   "string_of_t" >::: ["OCRA-1:HOTP-SHA512-0:C-QH16-T14H" >:: string_of_t];
+   "challenge" >::: ["OCRA-1:HOTP-SHA256-0:QA10" >:: challenge;
+                     "OCRA-1:HOTP-SHA256-0:QN10" >:: challenge;
+                     "OCRA-1:HOTP-SHA256-0:QH10" >:: challenge];
+   "gen" >::: ["OCRA-1:HOTP-SHA256-0:C-QH10" >:: gen_di_missmatch;
+               "OCRA-1:HOTP-SHA256-0:QH10-PSHA1" >:: gen_di_missmatch;
+               "OCRA-1:HOTP-SHA256-0:QH10-T1H" >:: gen_di_missmatch;
+               "OCRA-1:HOTP-SHA256-0:QH10-S123" >:: gen_di_missmatch;
+               "OCRA-1:HOTP-SHA1-0:QH10-S235" >:: gen_session_data;
+               "OCRA-1:HOTP-SHA1-10:QH10-S235" >:: gen_session_data;
+               "OCRA-1:HOTP-SHA1-10:QN08" >:: gen_cf_fill]]
 
 let suite =
   "All" >::: [ "known_answer" >::: known_answer;
