d8171219d07b8fdaf9bc284b9c27623b92a227ad
Stefan Grundmann <sg2342@googlemail.com>
2017-03-03 19:53:14 +0000

    conditional compilation of examples

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -4,6 +4,7 @@
 open Topkg
 
 let coverage = Conf.with_pkg ~default:false "coverage"
+let examples = Conf.with_pkg ~default:false "examples"
 
 let cmd c os files =
   let build =
@@ -17,12 +18,14 @@ let cmd c os files =
   OS.Cmd.run @@ Cmd.(build c os %% of_list files)
 
 let () =
-  let not_rfc6287_deps = Some ["bisect_ppx";"oUnit";"ounit"] in
+  let not_rfc6287_deps = Some ["bisect_ppx";"oUnit";"ounit";"cmdliner"] in
   let opams =
     [ Pkg.opam_file "opam" ~lint_deps_excluding:not_rfc6287_deps ]
   in
   Pkg.describe ~build:(Pkg.build ~cmd ()) ~opams "rfc6287" @@ fun c ->
+  let examples = Conf.value c examples in
   Ok [
     Pkg.mllib "lib/rfc6287.mllib";
     Pkg.test "test/tests";
+    Pkg.bin ~cond:examples "examples/ocra_tool"
   ]
