a0c39f5a1a156aca7a5a5f962693280b6efdbfa3
Stefan Grundmann <sg2342@googlemail.com>
2017-03-03 17:51:03 +0000

    working coverage

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -3,8 +3,25 @@
 #require "topkg"
 open Topkg
 
+let coverage = Conf.with_pkg ~default:false "coverage"
+
+let cmd c os files =
+  let build =
+    if Conf.value c coverage then
+      let coverage_arg = Cmd.(v "-pkg" % "bisect_ppx") in
+      let coverage_cmd c os = Cmd.(Pkg.build_cmd c os %% coverage_arg) in
+      coverage_cmd
+    else
+      Pkg.build_cmd
+  in
+  OS.Cmd.run @@ Cmd.(build c os %% of_list files)
+
 let () =
-  Pkg.describe "rfc6287" @@ fun c ->
+  let not_rfc6287_deps = Some ["bisect_ppx";"oUnit";"ounit"] in
+  let opams =
+    [ Pkg.opam_file "opam" ~lint_deps_excluding:not_rfc6287_deps ]
+  in
+  Pkg.describe ~build:(Pkg.build ~cmd ()) ~opams "rfc6287" @@ fun c ->
   Ok [
     Pkg.mllib "lib/rfc6287.mllib";
     Pkg.test "test/tests";
